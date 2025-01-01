7e280362df062169fe34e09a7c581956a630836d
Jonghyun <chlwhdgus86@gmail.com>
2025-01-01 23:52:54 +0900

    init commit

---
+++ src/main/java/io/github/hyunza/dianvicore/DianviCoreApplication.java
@@ -0,0 +1,13 @@
+package io.github.hyunza.dianvicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DianviCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DianviCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/hyunza/dianvicore/DianviCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hyunza.dianvicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DianviCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
