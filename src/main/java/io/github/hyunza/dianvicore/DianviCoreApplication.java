2568c6bf374b430c5e77c97ebb5759981ce7589d
Jonghyun <chlwhdgus86@gmail.com>
2025-01-11 19:49:55 +0900

    feat: log4j2 기능 추가

--- src/main/java/io/github/hyunza/dianvicore/DianviCoreApplication.java
+++ src/main/java/io/github/hyunza/dianvicore/DianviCoreApplication.java
@@ -1,13 +1,17 @@
 package io.github.hyunza.dianvicore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DianviCoreApplication {
+    private static Logger logger = LoggerFactory.getLogger(DianviCoreApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(DianviCoreApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(DianviCoreApplication.class);
+        springApplication.run(args);
+        logger.info("DianviCoreApplication started successfully");
     }
-
 }
